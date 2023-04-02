c08dbe9cb178e81132cbc3a713c996ca1cb87066
GabrielRID <gabrielrricardodavela@gmail.com>
2023-04-02 19:18:12 -0300

    tomara que de certo

--- src/component/scanner/Scanner.js
+++ src/component/scanner/Scanner.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import target from '../../assets/targets.mind'
+
 const Scanner = () => {
     return (
         <div style={{height: '100vh'}}>
-            <a-scene crossorigin="anonymous" mindar-image="imageTargetSrc: ../../targets.mind;" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+            <a-scene crossorigin="anonymous" mindar-image={`imageTargetSrc: ${target};`} color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
                 <a-assets>
                     <img id="card" crossorigin="anonymous" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.1/examples/image-tracking/assets/card-example/card.png" />
                     <a-asset-item id="avatarModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.1/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item>
