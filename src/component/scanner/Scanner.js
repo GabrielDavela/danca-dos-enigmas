8a22bce2404e7b85fca0a865ae84edea5f71e7cb
GabrielRID <gabrielrricardodavela@gmail.com>
2023-04-02 18:55:13 -0300

    .

--- src/component/scanner/Scanner.js
+++ src/component/scanner/Scanner.js
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet";
 const Scanner = () => {
     return (
         <div style={{height: '100vh'}}>
-            <a-scene mindar-image="imageTargetSrc: https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.1/examples/image-tracking/assets/card-example/card.mind;" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+            <a-scene mindar-image="imageTargetSrc: ../../../../../public/assets/targets.mind" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
                 <a-assets>
                     <img id="card" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.1/examples/image-tracking/assets/card-example/card.png" />
                     <a-asset-item id="avatarModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.1/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item>
