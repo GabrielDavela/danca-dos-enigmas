a566a8e0c8c1a0df984228648dcafe990ae87f43
GabrielRID <gabrielrricardodavela@gmail.com>
2023-04-02 19:14:58 -0300

    adicionando cross

--- src/component/scanner/Scanner.js
+++ src/component/scanner/Scanner.js
@@ -2,7 +2,7 @@ import React from 'react'
 const Scanner = () => {
     return (
         <div style={{height: '100vh'}}>
-            <a-scene mindar-image="imageTargetSrc: ../../targets.mind;" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+            <a-scene crossorigin="anonymous" mindar-image="imageTargetSrc: ../../targets.mind;" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
                 <a-assets>
                     <img id="card" crossorigin="anonymous" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.1/examples/image-tracking/assets/card-example/card.png" />
                     <a-asset-item id="avatarModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.1/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item>
