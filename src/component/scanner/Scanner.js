cee6c6e33f3fd61fedfe3829a86697624eaabc78
GabrielRID <gabrielrricardodavela@gmail.com>
2023-04-02 19:22:41 -0300

    adicionando carta01

--- src/component/scanner/Scanner.js
+++ src/component/scanner/Scanner.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import target from '../../assets/targets.mind'
+import carta_grupo1 from '../../assets/carta01.png'
 
 const Scanner = () => {
     return (
         <div style={{height: '100vh'}}>
             <a-scene crossorigin="anonymous" mindar-image={`imageTargetSrc: ${target};`} color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
                 <a-assets>
-                    <img id="card" crossorigin="anonymous" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.1/examples/image-tracking/assets/card-example/card.png" />
-                    <a-asset-item id="avatarModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.1/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item>
+                    <img id="card" crossorigin="anonymous" src={carta_grupo1} />
                 </a-assets>
 
                 <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
