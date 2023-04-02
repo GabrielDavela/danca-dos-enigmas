f9441442cda95fbc92f13cff1a3901dc514edf50
GabrielRID <gabrielrricardodavela@gmail.com>
2023-04-02 19:13:32 -0300

    .

--- src/component/scanner/Scanner.js
+++ src/component/scanner/Scanner.js
@@ -4,7 +4,7 @@ const Scanner = () => {
         <div style={{height: '100vh'}}>
             <a-scene mindar-image="imageTargetSrc: ../../targets.mind;" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
                 <a-assets>
-                    <img id="card" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.1/examples/image-tracking/assets/card-example/card.png" />
+                    <img id="card" crossorigin="anonymous" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.1/examples/image-tracking/assets/card-example/card.png" />
                     <a-asset-item id="avatarModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.1/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item>
                 </a-assets>
 
