6d2ab1a4c55e44cc54cbe9860fe178c22a13d949
GabrielRID <gabriel_davela@estudante.sesisenai.org.br>
2023-04-03 21:28:21 -0300

    Update Scanner.js

--- src/component/scanner/Scanner.js
+++ src/component/scanner/Scanner.js
@@ -11,9 +11,7 @@ const Scanner = (props) => {
 
     useEffect(() => {
         document.querySelector("#target-cards").addEventListener('targetFound', (e) => {
-            alert("Entrei aqui")
-            alert("achou algo " + e.detail.targetIndex)
-            setTargetIndex(e.detail.targetIndex)
+            setTargetIndex(e.target.index)
             alert(targetIndex)
         })
     }, [])
@@ -24,7 +22,7 @@ const Scanner = (props) => {
 
     return (
         <div style={{ height: '100vh' }}>
-            <div style={{ background: "#FFF" }}>teste: {targetIndex}</div>
+            <div style={{ background: "#FFF" }}>TESTE: {targetIndex}</div>
             <a-scene 
                 mindar-image={`imageTargetSrc: ${target};`}
                 color-space="sRGB"
