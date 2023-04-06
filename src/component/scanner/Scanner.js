7d00148c3e94aa861f5086986433f1298546a850
erikawk <105462871+erikawk@users.noreply.github.com>
2023-04-06 14:58:45 -0300

    testando os targets

--- src/component/scanner/Scanner.js
+++ src/component/scanner/Scanner.js
@@ -2,12 +2,11 @@ import React, { useContext, useEffect, useState } from 'react'
 import "./Scanner.css"
 
 import target from '../../assets/first-cards/first-cards.mind'
-import carta_grupo01 from '../../assets/first-cards/documents/carta-grupo-01.png'
+import carta_grupo01 from '../../assets/first-cards/documents/targets-gp-01.mind'
 import carta_grupo02 from '../../assets/first-cards/documents/carta-grupo-02.png'
 import carta_grupo03 from '../../assets/first-cards/documents/carta-grupo-03.png'
 import carta_grupo04 from '../../assets/first-cards/documents/carta-grupo-04.png'
 import { GameContext, readyPlayer } from '../../context/GameContext'
-
 
 
 const Scanner = ({ player }) => {
@@ -15,8 +14,6 @@ const Scanner = ({ player }) => {
     const [showButton, setShowButton] = useState(false)
     const [disabled, setDisabled] = useState(false)
     const { readyplayers } = useContext(GameContext)
-
-    const aline = "linda"; 
 
     document.addEventListener("targetFound", () => {
         setShowButton(true)
