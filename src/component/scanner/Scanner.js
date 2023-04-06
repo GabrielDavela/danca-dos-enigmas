655e848d0e53b3262674f403cb957a446bbd3f0c
erikawk <105462871+erikawk@users.noreply.github.com>
2023-04-06 16:20:51 -0300

    .

--- src/component/scanner/Scanner.js
+++ src/component/scanner/Scanner.js
@@ -68,56 +68,57 @@ const Scanner = ({ player }) => {
                     <a-plane src="#pista01Gp01" position="0 0 0" height="2.3" width="1.6" rotation="0 0 0"></a-plane>
                 </a-entity>
 
-
             </a-scene>
-            
-            <a-scene
-                mindar-image={`imageTargetSrc: ${target};`}
-                color-space="sRGB"
-                renderer="colorManagement: true, physicallyCorrectLights"
-                vr-mode-ui="enabled: false"
-                device-orientation-permission-ui="enabled: false"
-                id="target-cards"
-            >
-                <a-assets>
-                    <img id='groupCard01' src={carta_grupo01} />
-                    <img id='groupCard02' src={carta_grupo02} />
-                    <img id='groupCard03' src={carta_grupo03} />
-                    <img id='groupCard04' src={carta_grupo04} />
+
+            {false &&
+                <a-scene
+                    mindar-image={`imageTargetSrc: ${target};`}
+                    color-space="sRGB"
+                    renderer="colorManagement: true, physicallyCorrectLights"
+                    vr-mode-ui="enabled: false"
+                    device-orientation-permission-ui="enabled: false"
+                    id="target-cards"
+                >
+                    <a-assets>
+                        <img id='groupCard01' src={carta_grupo01} />
+                        <img id='groupCard02' src={carta_grupo02} />
+                        <img id='groupCard03' src={carta_grupo03} />
+                        <img id='groupCard04' src={carta_grupo04} />
 
 
-                </a-assets>
+                    </a-assets>
 
-                <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
-                {player.color.toLowerCase() === "vermelho" &&
-                    <a-entity mindar-image-target="targetIndex: 0">
-                        <a-plane src="#groupCard01" position="0 0 0" height="2.3" width="1.6" rotation="0 0 0"></a-plane>
-                    </a-entity>
-                }
+                    <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+                    {player.color.toLowerCase() === "vermelho" &&
+                        <a-entity mindar-image-target="targetIndex: 0">
+                            <a-plane src="#groupCard01" position="0 0 0" height="2.3" width="1.6" rotation="0 0 0"></a-plane>
+                        </a-entity>
+                    }
 
-                {player.color.toLowerCase() === "azul" &&
-                    <a-entity mindar-image-target="targetIndex: 1">
-                        <a-plane src="#groupCard02" position="0 0 0" height="2.3" width="1.6" rotation="0 0 0"></a-plane>
-                    </a-entity>
-                }
+                    {player.color.toLowerCase() === "azul" &&
+                        <a-entity mindar-image-target="targetIndex: 1">
+                            <a-plane src="#groupCard02" position="0 0 0" height="2.3" width="1.6" rotation="0 0 0"></a-plane>
+                        </a-entity>
+                    }
 
-                {player.color.toLowerCase() === "amarelo" &&
-                    <a-entity mindar-image-target="targetIndex: 2">
-                        <a-plane src="#groupCard03" position="0 0 0" height="2.3" width="1.6" rotation="0 0 0"></a-plane>
-                    </a-entity>
-                }
+                    {player.color.toLowerCase() === "amarelo" &&
+                        <a-entity mindar-image-target="targetIndex: 2">
+                            <a-plane src="#groupCard03" position="0 0 0" height="2.3" width="1.6" rotation="0 0 0"></a-plane>
+                        </a-entity>
+                    }
 
-                {player.color.toLowerCase() === "verde" &&
-                    <a-entity mindar-image-target="targetIndex: 3">
-                        <a-plane src="#groupCard04" position="0 0 0" height="2.3" width="1.6" rotation="0 0 0"></a-plane>
-                    </a-entity>
-                }
+                    {player.color.toLowerCase() === "verde" &&
+                        <a-entity mindar-image-target="targetIndex: 3">
+                            <a-plane src="#groupCard04" position="0 0 0" height="2.3" width="1.6" rotation="0 0 0"></a-plane>
+                        </a-entity>
+                    }
 
-                {!showButton &&
-                    <button className='button__scanner'
-                        onClick={ready} disabled={disabled}>Pronto<span>{readyplayers}/4</span></button>
-                }
-            </a-scene>
+                    {!showButton &&
+                        <button className='button__scanner'
+                            onClick={ready} disabled={disabled}>Pronto<span>{readyplayers}/4</span></button>
+                    }
+                </a-scene>
+            }
 
         </div>
     )
