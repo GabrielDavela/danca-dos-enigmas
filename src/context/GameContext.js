b046e341dce70b16706489dad93b278bbc11534f
Aline de Souza <gabrielrricardodavela@gmail.com>
2023-04-08 17:14:37 -0300

    Update GameContext.js

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useReducer } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
+const socket = io("https://server-enigma-production.up.railway.app/", {
     autoConnect: false
 })
+// const socket = io("http://localhost:4000", {
+//     autoConnect: false
+// })
 
 const GameContext = React.createContext()
 
