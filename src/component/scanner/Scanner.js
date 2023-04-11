262443176ef7744e90d0acf3c86218b7ee8bde8e
GabrielRID <gabriel_davela@estudante.sesisenai.org.br>
2023-04-11 18:07:17 -0300

    .

--- src/component/scanner/Scanner.js
+++ src/component/scanner/Scanner.js
@@ -37,7 +37,7 @@ const Scanner = () => {
                 </>
             }
 
-            {everyoneIsReady &&
+            {!everyoneIsReady &&
                 <FirstCards target={targets.first_cards} player={player} />
             }
 
