0d57af7fdefe9f0adf425acb85827ad62329fe6e
GabrielRID <gabriel_davela@estudante.sesisenai.org.br>
2023-05-02 21:14:44 -0300

    Update Home.js

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -25,9 +25,9 @@ const Home = () => {
         if(everyoneIsReady) timerGame(match)
     }, [everyoneIsReady])
 
-    useEffect(() => {
-        readyPlayer(false)
-    })
+    // useEffect(() => {
+    //     readyPlayer(false)
+    // })
 
     useEffect(() => {
         if ((hit.bool && hit.color === playerAux.color) || timer === "00:00") {
