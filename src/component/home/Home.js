01bcf147854e4d01178aec4f0a2d5e54531c859f
GabrielDavela <gabrielrricardodavela@gmail.com>
2023-05-04 10:08:08 -0300

    Update Home.js

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -114,7 +114,7 @@ const Home = () => {
                     }
                     <Scanner
                         player={playerAux}
-                        reload={() => setReloading()}
+                        reload={() => reload()}
                     />
                     {everyoneIsReady &&
                         <>
