f6580a1b7631dec68a5b238a93a11ee295474bff
GabrielDavela <gabrielrricardodavela@gmail.com>
2023-05-08 13:35:26 -0300

    .

--- src/component/scanner/groupScanners/FirstCards.js
+++ src/component/scanner/groupScanners/FirstCards.js
@@ -29,92 +29,25 @@ const FirstCard = ({ target, player }) => {
 
     return (
         <div className="container__image__firstCards">
-            {/* <a-scene
-                mindar-image={`imageTargetSrc: ${target};`}
-                color-space="sRGB"
-                renderer="colorManagement: false, physicallyCorrectLights"
-                vr-mode-ui="enabled: false"
-                device-orientation-permission-ui="enabled: false"
-                id="target-first-cards"
-            >
-                <a-assets>
-                    <img id="first-cards-group_01" src={firstcards.carta_grupo01} />
-                    <img id="first-cards-group_02" src={firstcards.carta_grupo02} />
-                    <img id="first-cards-group_03" src={firstcards.carta_grupo03} />
-                    <img id="first-cards-group_04" src={firstcards.carta_grupo04} />
-                </a-assets>
-
-                <a-camera position="0 0 0" look-controls="enabled: false" fov="80"></a-camera>
-
-                {player.color.toLowerCase() === "vermelho" && (
-                    <a-entity mindar-image-target="targetIndex: 0">
-                        <a-plane
-                            src="#first-cards-group_01"
-                            position="0 0 0"
-                            height="2.3"
-                            width="1.6"
-                            rotation="0 0 0"
-                        ></a-plane>
-                    </a-entity>
-                )}
-
-                {player.color.toLowerCase() === "azul" && (
-                    <a-entity mindar-image-target="targetIndex: 1">
-                        <a-plane
-                            src="#first-cards-group_02"
-                            position="0 0 0"
-                            height="2.3"
-                            width="1.6"
-                            rotation="0 0 0"
-                        ></a-plane>
-                    </a-entity>
-                )}
-
-                {player.color.toLowerCase() === "amarelo" && (
-                    <a-entity mindar-image-target="targetIndex: 2">
-                        <a-plane
-                            src="#first-cards-group_03"
-                            position="0 0 0"
-                            height="2.3"
-                            width="1.6"
-                            rotation="0 0 0"
-                        ></a-plane>
-                    </a-entity>
-                )}
-
-                {player.color.toLowerCase() === "verde" && (
-                    <a-entity mindar-image-target="targetIndex:3">
-                        <a-plane
-                            src="#first-cards-group_04"
-                            position="0 0 0"
-                            height="2.3"
-                            width="1.6"
-                            rotation="0 0 0"
-                        ></a-plane>
-                    </a-entity>
-                )} */}
 
             {player.color === "Vermelho" &&
-                <img src={firstcards.carta_grupo01} style={{ height: "80vh", width: "30%", marginBottom: "3.5rem" }} />
+                <img src={firstcards.carta_grupo01} className="image__firstCards" />
             }
             {player.color === "Azul" &&
-                <img src={firstcards.carta_grupo02} style={{ height: "80vh", width: "30%", marginBottom: "3.5rem" }} />
+                <img src={firstcards.carta_grupo02} className="image__firstCards" />
             }
             {player.color === "Amarelo" &&
-                <img src={firstcards.carta_grupo03} style={{ height: "80vh", width: "30%", marginBottom: "3.5rem" }} />
+                <img src={firstcards.carta_grupo03} className="image__firstCards" />
             }
             {player.color === "Verde" &&
-                <img src={firstcards.carta_grupo04} style={{ height: "80vh", width: "30%", marginBottom: "3.5rem" }} />
+                <img src={firstcards.carta_grupo04} className="image__firstCards" />
             }
-
 
             <button className='button__scanner'
                 onClick={onReady}
                 disabled={disabled}
             >Pronto<span>{readyplayers}/4</span>
             </button>
-
-            {/* </a-scene> */}
         </div>
     )
 }
